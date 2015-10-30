d5243d08bff70c338060fe6899e5ff0033be575a
case <case@casesandberg.com>
2015-10-30 10:49:52 -0500

    JSPM

---
+++ docs/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  React.createElement('div', {}, 'React Docs'),
+  document.getElementById('root')
+);
