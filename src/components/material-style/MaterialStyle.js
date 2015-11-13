83a1c3f83ede9da2c9767f797f9427ab962e033b
case <case@casesandberg.com>
2015-11-13 16:48:58 -0600

    `react-markdown` basic

--- src/components/material-style/MaterialStyle.js
+++ src/components/material-style/MaterialStyle.js
@@ -5,6 +5,27 @@ import ReactCSS from 'reactcss';
 
 import MaterialStyleSidebar from './MaterialStyleSidebar';
 import Breadcrumbs from '../common/Breadcrumbs';
+import Markdown from '../../../modules/react-markdown/src/index';
+
+const markdownTest = `
+# Welcome.
+
+The following Style Guide is a tool for developers and designers to use to ensure that a consistent design
+experience is maintained throughout GrubCentral. If you’re looking for either the visual design language or
+front-end code used across GrubCentral, you’re in the right place!
+
+Additionally, the living Style Guide is meant to grow and evolve alongside GrubCentral. As improvements are
+made across the platform, they’ll immediately be updated in the style guide so that you’ll always have the most
+up to date information.
+
+---
+
+### Be Principled
+
+As a GrubHub Product Designer, the following principles are in place to guide our design process and serve as
+solid footing for which all GrubHub products are built upon. The desire to create innovative products starts, and
+ends, with a mutual respect and understanding of our design principles.
+`;
 
 export class MaterialStyle extends ReactCSS.Component {
 
@@ -46,7 +67,7 @@ export class MaterialStyle extends ReactCSS.Component {
             <Breadcrumbs route={ this.props.route } />
           </div>
           <div style={ this.styles().body }>
-            Material Style
+            <Markdown text={ markdownTest }/>
           </div>
         </div>
       </div>
